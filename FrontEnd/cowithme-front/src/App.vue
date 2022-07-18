ffe1e4af29df42a16ee9559ad1d04a63dcec9049
te44382 <te44382@gmail.com>
2022-07-18 13:46:26 +0900

    init cowithme-front project

---
+++ FrontEnd/cowithme-front/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h2>{{ message }}</h2>
+		<button @click="increment">클릭</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const message = ref('COding WITH ME!');
+		const increment = () => {
+			message.value = message.value + '!';
+		};
+		return { message, increment };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
