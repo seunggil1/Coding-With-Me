143ddf571e1736fb55e8f44c75cc260aeaf9f7af
SeongMin Lee <62661142+realme1st@users.noreply.github.com>
2022-07-22 16:18:49 +0900

    Modify classes

--- BackEnd/src/main/java/com/ssafy/api/service/ClassesServiceImpl.java
+++ BackEnd/src/main/java/com/ssafy/api/service/ClassesServiceImpl.java
@@ -1,8 +1,13 @@
 package com.ssafy.api.service;
 
+//import com.ssafy.api.request.ClassesModifyPostReq;
+import com.ssafy.api.request.ClassesModifyPostReq;
 import com.ssafy.api.request.ClassesRegisterPostReq;
+
 import com.ssafy.db.entity.Classes;
+import com.ssafy.db.entity.User;
 import com.ssafy.db.repository.ClassesRepository;
+import com.ssafy.db.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +17,30 @@ public class ClassesServiceImpl implements ClassesService {
     @Autowired
     ClassesRepository classesRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public Classes createClass(ClassesRegisterPostReq classesRegisterInfo) {
         Classes classes =new Classes();
         classes.setClassName(classesRegisterInfo.getClassName());
         classes.setClassDescription(classesRegisterInfo.getClassDescription());
-
-
+        User user= userRepository.findByUserId(classesRegisterInfo.getTutorId()).get();
+        classes.setUser(user);
 
         return classesRepository.save(classes);
     }
+
+
+    @Override
+    public Classes modifyClass(ClassesModifyPostReq classesModifyPostReq) {
+        Classes classes = classesRepository.findByUserUserIdAndClassName(classesModifyPostReq.getTutorId(), classesModifyPostReq.getClassName()).get();
+
+        classes.setClassName(classesModifyPostReq.getNewclassName());
+        classes.setClassDescription(classesModifyPostReq.getClassDescription());
+
+        return classesRepository.save(classes);
+    }
+
+
 }
