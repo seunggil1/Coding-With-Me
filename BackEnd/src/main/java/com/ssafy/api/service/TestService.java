82f9960145bf9d004972e5cb413ac1a13befdddf
SeongMin Lee <62661142+realme1st@users.noreply.github.com>
2022-07-27 00:47:14 +0900

    add modifyTestInfo

--- BackEnd/src/main/java/com/ssafy/api/service/TestService.java
+++ BackEnd/src/main/java/com/ssafy/api/service/TestService.java
@@ -1,9 +1,13 @@
 package com.ssafy.api.service;
 
+import com.ssafy.api.request.TestModifyPostReq;
 import com.ssafy.api.request.TestRegisterPostReq;
 import com.ssafy.db.entity.Test;
 
 public interface TestService {
     Test createTest(TestRegisterPostReq testRegisterPostReq);
 
+    Test modifyTest(TestModifyPostReq testModifyPostReq);
+
+    boolean deleteTest(TestRegisterPostReq testRegisterPostReq);
 }
