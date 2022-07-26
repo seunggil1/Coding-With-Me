a26926fc2ceb12749ea6a596f5477f3b7000143a
김동욱 <kdw508@naver.com>
2022-07-26 23:55:01 +0900

    add conference api

--- BackEnd/src/main/java/com/ssafy/db/repository/ClassesRepository.java
+++ BackEnd/src/main/java/com/ssafy/db/repository/ClassesRepository.java
@@ -13,4 +13,6 @@ public interface ClassesRepository extends JpaRepository <Classes, Long>{
     Optional<Classes> findByUserUserIdAndClassName(Long user, String classname);
 
     Optional<List<Classes>> findByUserUserId(Long user);
+
+    Optional<Classes> findByClassId(Long classId);
 }
