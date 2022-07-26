6d5e00c1deec384ad2db8ee08ce7b2faaa22372e
herrinekim <herrine.kim@gmail.com>
2022-07-27 00:50:45 +0900

    add pinia login methods and quasar form

--- FrontEnd/cowithme-front/src/components/organisms/login/RightForm.vue
+++ FrontEnd/cowithme-front/src/components/organisms/login/RightForm.vue
@@ -2,37 +2,88 @@
 	<div class="right">
 		<div class="container">
 			<LoginText class="component"></LoginText>
-			<div class="form-container">
-				<IDInput class="component"></IDInput>
-				<PWInput class="component"></PWInput>
+			<div id="q-app" style="min-height: 100vh">
+				<div class="q-pa-md" style="max-width: 400px">
+					<q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+						<q-input
+							filled
+							v-model="id"
+							label="Your name *"
+							hint="Name and surname"
+							lazy-rules
+							:rules="[
+								val => (val && val.length > 0) || 'Please type something',
+							]"
+						></q-input>
+
+						<q-input
+							filled
+							type="number"
+							v-model="age"
+							label="Your age *"
+							lazy-rules
+							:rules="[
+								val => (val !== null && val !== '') || 'Please type your age',
+								val => (val > 0 && val < 100) || 'Please type a real age',
+							]"
+						></q-input>
+
+						<div>
+							<q-btn label="Submit" type="submit" color="primary"></q-btn>
+							<q-btn
+								label="Reset"
+								type="reset"
+								color="primary"
+								flat
+								class="q-ml-sm"
+							></q-btn>
+						</div>
+					</q-form>
+				</div>
+			</div>
+
+			<!-- <div class="form-container">
+				<IDInput name="id" class="component"></IDInput>
+				<PWInput name="password" class="component"></PWInput>
 			</div>
 			<FindIDPW class="find"></FindIDPW>
-			<LoginButton class="login-button"></LoginButton>
-			<SelectRoleButton class="role-button"></SelectRoleButton>
+			<LoginButton class="login-button"></LoginButton> -->
 		</div>
 	</div>
 </template>
 
 <script>
+import { useAuthStore } from 'src/stores';
+import { ref } from 'vue';
+
 import LoginText from 'src/components/molecules/login/LoginText.vue';
-import IDInput from 'src/components/molecules/login/IDInput.vue';
-import PWInput from 'src/components/molecules/login/PWInput.vue';
-import FindIDPW from 'src/components/molecules/login/FindIDPW.vue';
-import LoginButton from 'src/components/molecules/login/LoginButton.vue';
-import SelectRoleButton from 'src/components/molecules/login/SelectRoleButton.vue';
+// import IDInput from 'src/components/molecules/login/IDInput.vue';
+// import PWInput from 'src/components/molecules/login/PWInput.vue';
+// import FindIDPW from 'src/components/molecules/login/FindIDPW.vue';
+// import LoginButton from 'src/components/molecules/login/LoginButton.vue';
+// import SelectRoleButton from 'src/components/molecules/login/SelectRoleButton.vue';
 
 export default {
 	name: 'RightForm',
 	setup() {
-		return {};
+		const id = ref(null);
+		const age = ref(null);
+		const accept = ref(false);
+
+		async function onSubmit(values) {
+			const authStore = useAuthStore();
+			const { id, password } = values;
+			await authStore.login(id, password);
+		}
+		return { id, age, accept };
 	},
 	components: {
 		LoginText,
-		IDInput,
-		PWInput,
-		FindIDPW,
-		LoginButton,
-		SelectRoleButton,
+		// IDInput,
+		// PWInput,
+		// FindIDPW,
+		// LoginButton,
+		// SelectRoleButton,
 	},
 };
 </script>
---
+++ FrontEnd/cowithme-front/src/stores/auth.store.js
@@ -0,0 +1,45 @@
+import { defineStore } from 'pinia';
+
+import { fetchWrapper } from 'src/helpers';
+import { router } from 'src/router';
+import { useAlertStore } from 'src/stores';
+
+const HOST = 'http://localhost:8080/api/v1';
+
+const baseUrl = `${HOST}/users`;
+
+export const useAuthStore = defineStore({
+	id: 'auth',
+	state: () => ({
+		// initialize state from local storage to enable user to stay logged in
+		user: JSON.parse(localStorage.getItem('user')),
+		returnUrl: null,
+	}),
+	actions: {
+		async login(id, password) {
+			try {
+				const user = await fetchWrapper.post(`${baseUrl}/auth/login`, {
+					id,
+					password,
+				});
+
+				// update pinia state
+				this.user = user;
+
+				// store user details and jwt in local storage to keep user logged in between page refreshes
+				localStorage.setItem('user', JSON.stringify(user));
+
+				// redirect to previous url or default to home page
+				router.push(this.returnUrl || '/');
+			} catch (error) {
+				const alertStore = useAlertStore();
+				alertStore.error(error);
+			}
+		},
+		logout() {
+			this.user = null;
+			localStorage.removeItem('user');
+			router.push('/login');
+		},
+	},
+});
