5063100533b491266d761d761b8a3ae19ba1e242
SSAFY <kdw508@naver.com>
2022-07-27 17:04:35 +0900

    fix TestRegisterPostReq

--- BackEnd/src/main/java/com/ssafy/api/request/TestRegisterPostReq.java
+++ BackEnd/src/main/java/com/ssafy/api/request/TestRegisterPostReq.java
@@ -5,6 +5,9 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 @ApiModel("TestPostReq")
@@ -13,10 +16,26 @@ public class TestRegisterPostReq {
     Long classId;
     @ApiModelProperty(name = "제목", example = "다익스트라 응용문제")
     String testName;
+
     @ApiModelProperty(name = "문항 수", example = "3")
     int testQno;
+
     @ApiModelProperty(name="경로", example= "/path/..")
     String testPath;
+
     @ApiModelProperty(name="테스트케이스",example="..")
-    String testcase;
+    List<testcase> testcaseList;
+
+    @Getter
+    @Setter
+    public static class testcase{
+        int qno;
+        List<IO> IOList = new ArrayList<>();
+    }
+    @Getter
+    @Setter
+    public static class IO{
+        String input;
+        String output;
+    }
 }
