92cd7f8d0f3cac60aedfde29b860694495cdff31
Hyerin Kim <aprilkim98@naver.com>
2022-07-29 16:10:24 +0900

    fix signup error

--- FrontEnd/cowithme-front/src/components/organisms/signup/BaseSquare.vue
+++ FrontEnd/cowithme-front/src/components/organisms/signup/BaseSquare.vue
@@ -17,11 +17,21 @@ const phone = ref('');
 const nickname = ref('');
 const birthDt = ref('');
 
-async function onSubmit(values) {
+async function onSubmit() {
+	let user = {
+		name: name.value,
+		id: id.value,
+		password: password.value,
+		email: email.value,
+		phone: phone.value,
+		nickname: nickname.value,
+		birthDt: birthDt.value,
+		role: role.value,
+	};
 	const usersStore = useUsersStore();
 	const alertStore = useAlertStore();
 	try {
-		await usersStore.register(values);
+		await usersStore.register(user);
 		await router.push('/login');
 		alertStore.success('Registration successful');
 	} catch (error) {
@@ -144,7 +154,6 @@ function onReset() {
 						class="q-ma-lg q-pr-lg"
 						rounded
 						outlined
-						type="text"
 						v-model="nickname"
 						label="닉네임"
 						lazy-rules
@@ -157,7 +166,6 @@ function onReset() {
 						rounded
 						outlined
 						label="생일"
-						type="text"
 						v-model="birthDt"
 						lazy-rules
 						color="brand"
@@ -168,7 +176,6 @@ function onReset() {
 						class="q-ma-lg q-pr-lg"
 						rounded
 						outlined
-						type="text"
 						label="role"
 						v-model="role"
 						lazy-rules
@@ -224,6 +231,59 @@ function onReset() {
 			</div>
 		</q-form>
 	</div>
+	<!-- <div class="card m-3">
+		<h4 class="card-header">회원가입</h4>
+		<div class="card-body">
+			<Form @submit="onSubmit" :validation-schema="schema">
+				<div class="form-group">
+					<label>생년월일</label>
+					<Field name="birthDt" type="text" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label>email</label>
+					<Field name="email" type="text" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label>id</label>
+					<Field name="id" type="text" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label>name</label>
+					<Field name="name" type="text" class="form-control" />
+				</div>
+				<div class="form-group">
+					<button class="btn btn-primary" :disabled="isSubmitting">
+						<span
+							v-show="isSubmitting"
+							class="spinner-border spinner-border-sm mr-1"
+						></span>
+						Register
+					</button>
+					<router-link to="login" class="btn btn-link">Cancel</router-link>
+				</div>
+				<div class="form-group">
+					<label>nickname</label>
+					<Field name="nickname" type="text" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label>Username</label>
+					<Field name="username" type="text" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label>password</label>
+					<Field name="password" type="text" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label>phone</label>
+					<Field name="phone" type="text" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label>role</label>
+					<Field name="role" type="text" class="form-control" />
+				</div>
+			</Form>
+		</div>
+	</div> -->
 </template>
 
 <style>
