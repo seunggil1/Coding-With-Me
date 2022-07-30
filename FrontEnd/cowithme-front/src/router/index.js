ea88462f96e6ebc1c7a8a81ff7fe6e4e89d5bd41
Hyerin Kim <aprilkim98@naver.com>
2022-07-30 22:15:10 +0900

    install vue-router 4.x

--- FrontEnd/cowithme-front/src/router/index.js
+++ FrontEnd/cowithme-front/src/router/index.js
@@ -7,8 +7,6 @@ import {
 } from 'vue-router';
 import routes from './routes';
 
-export * as router from './routes.js';
-
 /*
  * If not building with SSR mode, you can
  * directly export the Router instantiation;
