c979bc098ea5250168696e836d1ddefacd889f06
Hyerin Kim <aprilkim98@naver.com>
2022-07-31 16:58:45 +0900

    change dirs

--- FrontEnd/cowithme-front/src/router/routes.js
+++ FrontEnd/cowithme-front/src/router/routes.js
@@ -4,38 +4,33 @@ const routes = [
 		component: () => import('layouts/MainLayout.vue'),
 		children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
 	},
-
-	// Always leave this as last one,
-	// but you can also remove it
-	{
-		path: '/teacherHome',
-		component: () => import('pages/TeacherHomePage.vue'),
-	},
-	{
-		path: '/:catchAll(.*)*',
-		component: () => import('pages/ErrorNotFound.vue'),
-	},
 	// 로그인/회원가입
 	{
 		path: '/login',
-		component: () => import('pages/LoginPage.vue'),
+		component: () => import('pages/account/LoginPage.vue'),
 	},
 	{
 		path: '/IDfind',
-		component: () => import('pages/IDFindPage.vue'),
+		component: () => import('pages/account/IDFindPage.vue'),
 	},
 	{
 		path: '/PWfind',
-		component: () => import('pages/PWFindPage.vue'),
+		component: () => import('pages/account/PWFindPage.vue'),
 	},
 	{
 		path: '/join',
-		component: () => import('pages/SignUpPage.vue'),
+		component: () => import('pages/account/SignUpPage.vue'),
 	},
 	// home
 	{
 		path: '/home',
 		component: () => import('pages/StudentHomePage.vue'),
+	},
+	// Always leave this as last one,
+	// but you can also remove it
+	{
+		path: '/:catchAll(.*)*',
+		component: () => import('pages/ErrorNotFound.vue'),
 	},
 ];
 
