24e4fc30c9d9031f4be7b1f8d431a66b2197f136
Hyerin Kim <aprilkim98@naver.com>
2022-08-03 17:56:45 +0900

    addING auth redirecting

--- FrontEnd/cowithme-front/src/router/index.js
+++ FrontEnd/cowithme-front/src/router/index.js
@@ -6,6 +6,7 @@ import {
 	createWebHashHistory,
 } from 'vue-router';
 import routes from './routes';
+import { useAuthStore } from 'src/stores';
 
 /*
  * If not building with SSR mode, you can
@@ -31,6 +32,18 @@ export default route(function (/* { store, ssrContext } */) {
 		// quasar.conf.js -> build -> vueRouterMode
 		// quasar.conf.js -> build -> publicPath
 		history: createHistory(process.env.VUE_ROUTER_BASE),
+	});
+
+	Router.beforeEach(async to => {
+		// redirect to login page if not logged in and trying to access a restricted page
+		const publicPages = ['/login'];
+		const authRequired = !publicPages.includes(to.path);
+		const auth = useAuthStore();
+
+		if (authRequired && !auth.user) {
+			auth.returnUrl = to.fullPath;
+			return '/login';
+		}
 	});
 
 	return Router;
