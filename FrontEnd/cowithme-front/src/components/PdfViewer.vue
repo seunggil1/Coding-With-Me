2fe70f9f33aa3843f034b1c2288484db1c6ad3b8
강승길 <5157-ksgg1navercom@users.noreply.lab.ssafy.com>
2022-08-04 17:10:11 +0900

    Add PdfView

---
+++ FrontEnd/cowithme-front/src/components/PdfViewer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <vue-pdf-embed :source="source">
+            
+        </vue-pdf-embed>
+    </div>
+</template>
+
+<script>
+import VuePdfEmbed from 'vue-pdf-embed';
+
+// example
+// <pdf-viewer source="https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK"></pdf-viewer>
+export default {
+    components : {
+        VuePdfEmbed
+    },
+    props : {
+        source : String
+    },
+    setup (props) {
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- FrontEnd/cowithme-front/src/components/TestComp.vue
+++ FrontEnd/cowithme-front/src/components/TestComp.vue
@@ -1,33 +1,19 @@
 <template>
-	<smart-widget-grid :layout="layout">
-		<!-- layout[$].i as slot name -->
-		<template #0>
-			<smart-widget simple>
-				<div class="layout-center">
-					<h3>Simple Widget Without Header</h3>
-				</div>
-			</smart-widget>
-		</template>
-		<template #1>
-			<smart-widget title="Default Widget">
-				<div class="layout-center">
-					<h3>Default Widget With Header</h3>
-				</div>
-			</smart-widget>
-		</template>
-		<template #2>
-			<smart-widget title="Full Screen" fullscreen>
-				<div class="layout-center">
-					<h3>Make any widget full screen</h3>
-				</div>
-			</smart-widget>
-		</template>
-	</smart-widget-grid>
+	<div>
+		<pdf-viewer source="https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK"></pdf-viewer>
+	</div>
 </template>
 
-<script setup>
+<script>
 // import { SmartWidget, SmartWidgetGrid } from 'src/boot/smart.js';
 
+import PdfViewer from './PdfViewer.vue';
+
+export default {
+	components: {
+		PdfViewer
+	},
+};
 const layout = [
 	{ x: 0, y: 0, w: 4, h: 4, i: '0' },
 	{ x: 4, y: 0, w: 4, h: 4, i: '1' },
