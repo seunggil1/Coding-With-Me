a9feb48ad20ad8208df5f16cfc1ee120111c35ec
herrinekim <herrine.kim@gmail.com>
2022-08-05 17:27:39 +0900

    fix hot-reloading

--- FrontEnd/cowithme-front/src/pages/teacher/MakeExamPage.vue
+++ FrontEnd/cowithme-front/src/pages/teacher/MakeExamPage.vue
@@ -1,6 +1,30 @@
 <template>
-	<div>시험 생성 form</div>
-	<p>{{ classId }}</p>
+	<div>
+		<p>시험 생성 form</p>
+		<p>{{ classId }}</p>
+		<div class="q-pa-md" style="max-width: 400px">
+			<q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+				<q-input
+					filled
+					v-model="name"
+					label="Your name *"
+					hint="Name and surname"
+					lazy-rules
+					:rules="[val => (val && val.length > 0) || 'Please type something']"
+				></q-input>
+				<div>
+					<q-btn label="Submit" type="submit" color="primary"></q-btn>
+					<q-btn
+						label="Reset"
+						type="reset"
+						color="primary"
+						flat
+						class="q-ml-sm"
+					></q-btn>
+				</div>
+			</q-form>
+		</div>
+	</div>
 </template>
 
 <script>
