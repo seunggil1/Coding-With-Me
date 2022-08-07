bfa22d2ba4bbf5b68d24cf20fee1c7b6193b0ae6
herrinekim <herrine.kim@gmail.com>
2022-08-07 14:12:21 +0900

    fix class detail page props not readable

--- FrontEnd/cowithme-front/src/pages/teacher/ClassDetailPage.vue
+++ FrontEnd/cowithme-front/src/pages/teacher/ClassDetailPage.vue
@@ -1,17 +1,18 @@
 <template>
-	<div class="q-pa-md q-gutter-md">
-		<q-btn>개나리반 의 강의 생성하자 룰루</q-btn>
+	<div class="q-pa-md">
+		<q-btn>{{ className }}의 강의 생성하기</q-btn>
 		<!-- <VueLogo></VueLogo> -->
-		<div class="box2">
+		<div class="box2 q-mb-md">
 			<p>강의 목록</p>
 			<q-btn>강의 시작(WebRTC 토큰 설정되면)</q-btn>
 		</div>
-		<div class="row box">
-			<div class="col-6">
+		<div class="flex row">
+			<div class="box col-6">
 				학생 목록 및 추가
 				<q-btn @click="goAddStudent">학생 추가</q-btn>
 			</div>
-			<div class="col-6">
+			<div class="q-ma-sm"></div>
+			<div class="box col-6">
 				시험 목록 및 생성
 				<router-link :to="{ name: 'makeExam', params: { classId: classId } }">
 					<q-btn>시험 생성</q-btn>
@@ -86,8 +87,8 @@ export default {
 
 <style scoped>
 .box {
-	height: 300px;
-	width: 100%;
+	min-height: 300px;
+	/* width: 100%; */
 	background-color: white;
 	border-radius: 10px;
 	box-shadow: 3px 3px 3px 2px rgba(0, 0, 0, 0.2);
