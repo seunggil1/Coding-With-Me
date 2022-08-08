4c50f51d3e30cc077e1425df9258a2b4a5201c5d
seung <38664481+seunggil1@users.noreply.github.com>
2022-08-08 18:05:17 +0900

    auto save

--- FrontEnd/WebRTC/quasar-project/src/router/routes.js
+++ FrontEnd/WebRTC/quasar-project/src/router/routes.js
@@ -1,6 +1,6 @@
 import MainLayout from 'layouts/MainLayout.vue';
 import VideoLayout from 'layouts/VideoLayout.vue';
-import LectureLayout from 'src/layouts/StudentLectureLayout.vue';
+import TeacherLectureLayout from 'src/layouts/TeacherLectureLayout.vue';
 import TeacherExamLayout from 'src/layouts/TeacherExamLayout.vue';
 import StudentExamLayout from 'src/layouts/StudentExamLayout.vue';
 
@@ -15,7 +15,7 @@ const routes = [
   },
   {
     path: '/lecture',
-    component: LectureLayout,
+    component: TeacherLectureLayout,
   },
   {
     path: '/exam',
