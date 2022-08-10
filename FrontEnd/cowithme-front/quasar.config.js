80323cc595f960078c441135323016eb22b686a3
herrinekim <herrine.kim@gmail.com>
2022-08-10 10:38:59 +0900

    fix history to hash

--- FrontEnd/cowithme-front/quasar.config.js
+++ FrontEnd/cowithme-front/quasar.config.js
@@ -47,7 +47,7 @@ module.exports = configure(function (/* ctx */) {
 				node: 'node16',
 			},
 
-			vueRouterMode: 'history', // available values: 'hash', 'history'
+			vueRouterMode: 'hash', // available values: 'hash', 'history'
 			// vueRouterBase,
 			// vueDevtools,
 			// vueOptionsAPI: false,
