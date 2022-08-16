adbd689e089fcc16477f9c51582f9fbe58a29bd6
te44382 <te44382@gmail.com>
2022-08-16 12:04:33 +0900

    add loading animation & refresh button

---
+++ FrontEnd/cowithme-front/src/pages/webRTC/LoadingPage.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="main-container row justify-center items-center">
+		<Vue3Lottie
+			:animationData="LoadingJSON"
+			:height="800"
+			:width="800"
+			:speed="1.5"
+		/>
+	</div>
+</template>
+
+<script>
+import { Vue3Lottie } from 'vue3-lottie';
+import 'vue3-lottie/dist/style.css';
+import LoadingJSON from 'src/assets/loading/loading.json';
+
+export default {
+	components: {
+		Vue3Lottie,
+	},
+	setup() {
+		return { LoadingJSON };
+	},
+};
+</script>
+
+<style scoped>
+.main-container {
+	height: 100vh !important;
+	width: 100vw !important;
+	/* background-color: #303841 !important; */
+}
+</style>
